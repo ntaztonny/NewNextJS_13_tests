63a9b982535bfa8da6c9baeea4740fa7594dc42f
ntaztonny <atozpp@yahoo.com>
2025-09-05 19:44:54 +0200

    example add multiple tests

--- nextjs_13_beta_tests/utils/utils.test.ts
+++ nextjs_13_beta_tests/utils/utils.test.ts
@@ -1,7 +1,19 @@
 import { add } from "./utils";
 
 describe("add", () => {
-  it("adds two numbers", () => {
+  it("should return the sum of two positive numbers", () => {
     expect(add(2, 3)).toBe(5);
+  });
+
+  it("should return the sum of a positive and negative number", () => {
+    expect(add(5, -2)).toBe(3);
+  });
+
+  it("should return zero when adding zero and zero", () => {
+    expect(add(0, 0)).toBe(0);
+  });
+
+  it("should handle large numbers correctly", () => {
+    expect(add(1000000, 2000000)).toBe(3000000);
   });
 });
